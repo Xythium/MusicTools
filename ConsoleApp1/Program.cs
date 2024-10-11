b779d997bff76b1694766c4c409a4a5f2210563f
Mark Bakker <xythium@gmail.com>
2024-10-11 18:49:43 +0200

    1.4.8.8

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MusicTools.Parsing.Subgenre;
+using MusicTools.Utils;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* var parser = new SubgenreParser();
+ 
+             parser.Parse("Bass House > Tech Trance");*/
+
+            Console.WriteLine(JsonConvert.SerializeObject(SubgenresUtils.Parse("Bass House > Tech Trance"), Formatting.Indented));
+            ;
+        }
+    }
+}
---
+++ MusicToolTests/GenreParserTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MusicTools.Parsing.Subgenre;
+using MusicTools.Utils;
+using NUnit.Framework;
+
+namespace MusicToolTests;
+
+public class GenreParserTests
+{
+    private SubgenreParser parser;
+
+    [SetUp]
+    public void Setup() { parser = new SubgenreParser(); }
+
+    [TestCaseSource(typeof(Data), nameof(Data.Cases))]
+    public List<string> Subgenre(string input) { return SubgenresUtils.SplitSubgenres(input); }
+}
+
+public class Data
+{
+    public static IEnumerable Cases
+    {
+        get
+        {
+            yield return new TestCaseData("Melodic Dubstep ~ Brostep").Returns(new List<string>
+            {
+                "Melodic Dubstep",
+                "Brostep"
+            });
+        }
+    }
+}
